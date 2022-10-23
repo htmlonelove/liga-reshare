13e498330bd224df286d96a649bf28d4c835a361
ExTeeP <exteep@yandex.ru>
2022-10-23 14:09:50 +0300

    Добавляет компонент Адаптивного бегунка

---
+++ source/components/adaptive-runner/js/init-adaptive-runner.js
@@ -0,0 +1,8 @@
+import {AdaptiveRunner} from './adaptive-runner';
+
+const initAdaptiveRunner = () => {
+  // eslint-disable-next-line no-unused-vars
+  const adaptiveRunner = new AdaptiveRunner();
+};
+
+export {initAdaptiveRunner};
